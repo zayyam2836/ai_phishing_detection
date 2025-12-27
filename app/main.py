fbb2114a2f1b16ec020f264ce69cf13aa7d86763
zayyam2836 <legendarydjdon786@gmail.com>
2025-12-27 17:37:33 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -171,5 +171,6 @@ def main():
         
         input("\nPress Enter to continue...")
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
